4cf315ad31f04ed3e187e589f975d43d51662c02
saan-saurabh <saurabhanshraj@gmail.com>
2020-03-28 00:46:34 +0530

    Updates

---
+++ precache-manifest.9245f8026e8c20ed8b86dccc4c758416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e6ad34455fa0478865573aaada2dd1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5c519806ceaa6d3c9f0f",
+    "url": "/my-app/static/css/main.5ff2fe81.chunk.css"
+  },
+  {
+    "revision": "b5331aebe430b865752e",
+    "url": "/my-app/static/js/2.49146f18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.49146f18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c519806ceaa6d3c9f0f",
+    "url": "/my-app/static/js/main.a704e41c.chunk.js"
+  },
+  {
+    "revision": "4bef99b01b1198237996",
+    "url": "/my-app/static/js/runtime-main.ea1c5a70.js"
+  }
+]);
