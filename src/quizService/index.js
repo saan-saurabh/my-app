5092619d92716ab791950d397df5fe64be633ee2
saan-saurabh <saurabhanshraj@gmail.com>
2020-03-28 00:39:12 +0530

    initial commit

---
+++ src/quizService/index.js
@@ -0,0 +1,373 @@
+const qBank = [
+  {
+    question:
+      "Virat kohli is associated with which game?   ",
+    answers: ["Soccer", "Badminton", "Cricket", "Tennis"],
+    correct: "Cricket",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Where is the train station "Llanfair­pwllgwyngyll­gogery­chwyrn­drobwll­llan­tysilio­gogo­goch"?',
+    answers: ["Wales", "Moldova", "Czech Republic", "Denmark"],
+    correct: "Wales",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Who is the CEO of GOOGLE?",
+    answers: ["Sundar Pichai", "Satya Nadela", "Rahul Sharma", "Bill Gates"],
+    correct: "Sundar Pichai",
+    questionId: "267908"
+  },
+  {
+    question: "Alexa is a product from which Company?",
+    answers: [
+      "Amazon",
+      "Google",
+      "Microsoft",
+      "IBM "
+    ],
+    correct: "Amazon",
+    questionId: "333247"
+  },
+  {
+    question: "The classical movie Dil wale dulhania le jayenge is commonly known as ?",
+    answers: ["DDLJ", "DilDLJ", "DDLjayenge", "DDJ"],
+    correct: "DDLJ",
+    questionId: "496293"
+  },
+  {
+    question: "Mark Zuckerburg owns?",
+    answers: [
+      "Whatsapp",
+      "Skype",
+      "Facebook",
+      "IBM"
+    ],
+    correct: "Facebook",
+    questionId: "588909"
+  },
+  {
+    question:"Sawai Man Singh stadium is situated in?",
+    answers: ["Jalandhar", "Jaipur", "Bombay", "Chandigarh"],
+    correct: "Jaipur",
+    questionId: "648452"
+  },
+  {
+    question: "Jaipur is known as?",
+    answers: ["Pink City", "Blue City", "Pearl City", "Black Diamond"],
+    correct: "Pink City",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Aligarh is famous for?",
+    answers: ["Key", "Jwellery", "Locks", "Cellphone"],
+    correct: "Locks",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Which one of the following is based on one of Chetan Bhagat's best selling book?",
+    answers: [
+      "Sultan",
+      "Dangal",
+      "He & Me",
+      "Half Girlfriend"
+    ],
+    correct: "Half Girlfriend",
+    questionId: "98390"
+  },
+  {
+    question: "Capital of Goa is ?",
+    answers: ["Panji", "Mumbai", "Chandigarh", "Ballia"],
+    correct: "Panji",
+    questionId: "1071006"
+  },
+  {
+    question: "In past times, what would a gentleman keep in his fob pocket?",
+    answers: ["Watch", "Money", "Keys", "Notebook"],
+    correct: "Watch",
+    questionId: "1174154"
+  },
+  {
+    question: "Which one of the following is based on one of Chetan Bhagat's best selling book?",
+    answers: ["2 state", "Arizona", "New york", "Utah"],
+    correct: "2 state",
+    questionId: "1226535"
+  },
+  {
+    question: "How would one say goodbye in Spanish?",
+    answers: ["Adiós", " Hola", "Au Revoir", "Salir"],
+    correct: "Adiós",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Skin", "Heart", "large Intestine", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "When we reverse the letters of Saas we will get ?",
+    answers: ["Asas", "Aass", "Saas", "Sasa"],
+    correct: "Saas",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Who played the role of Basanti in Movie Sholay?",
+    answers: ["Hema Malini", "Madhuri Dixit", "Tabbu", "Priyanka Chopra"],
+    correct: "Hema Malini",
+    questionId: "1822532"
+  },
+  {
+    question: "Tik Tok is a social media handle which is famous for ?",
+    answers: [
+      "Video clips",
+      "News",
+      "Chatting",
+      "Shopping"
+    ],
+    correct: "Video clips",
+    questionId: "195075"
+  },
+  {
+    question: '"Wo bulati hai magar jane ka nahi" is written by?',
+    answers: ["Rahat Indori", "Zubair ali", "Zakir khan ", "Not known"],
+    correct: "Rahat Indori",
+    questionId: "2019778"
+  },
+  {
+    question: "THIRD comes after ?",
+    answers: ["Third", "Fourth", "Fifth", "Second"],
+    correct: "Second",
+    questionId: "2134343"
+  },
+  {
+    question: ' Movie "Super 30" is based on the life of?',
+    answers: [
+      "Anand Kumar",
+      "Meat Beat Mania",
+      "Hrithik Roshan",
+      "Salman Khan"
+    ],
+    correct: "Anand Kumar",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Where is the Sardar Vallabhbhai Patel stadium situated?",
+    answers: ["Ahemdabad", "Gujrat", "Rajasthan", "Lucknow"],
+    correct: "Ahemdabad",
+    questionId: "2379831"
+  },
+  {
+    question: '"Nabawo ka sehar" synonyms with which city of India?',
+    answers: ["Delhi", "Jaipur", "Kanpur", "Lucknow"],
+    correct: "Lucknow",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the nickname of the US state of California?",
+    answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: "What is on display in the Madame Tussaud's museum in London?",
+    answers: [
+      "Wax sculptures",
+      "Designer clothing",
+      "Unreleased film reels",
+      "Vintage cars"
+    ],
+    correct: "Wax sculptures",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Indian currency has a picture of ?",
+    answers: ["Mahatma Gandhi", "Narendra Modi", "Amit Shah", "Sachin Tendulkar"],
+    correct: "Mahatma Gandhi",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Pink", "Yellow", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: "CEO of Amazon is?",
+    answers: ["Pichai", "Kalin Bhaiya", " Jeff Bezos", " Bill gates"],
+    correct: "Jeff Bezos",
+    questionId: "298060"
+  },
+  {
+    question: "kalin Bhaiya was a famous character of which Indian Web series?",
+    answers: ["Mirjapur", "Familyman", "Broken heart", "Behad"],
+    correct: "Mirjapur",
+    questionId: "3096579"
+  },
+  {
+    question: "Who is depicted on the US hundred dollar bill?",
+    answers: [
+      "Benjamin Franklin",
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "Benjamin Franklin",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Which one of these indian city is not having Metro facility?",
+    answers: ["Jaipur", "Delhi", "Kolkata", "Dehradoon"],
+    correct: "Dehradoon",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
+    answers: ["Truth", "Answer", "Cause", "Source"],
+    correct: "Truth",
+    questionId: "3410327"
+  },
+  {
+    question: "It is a sacred place of Sikhs in Amritsar?",
+    answers: ["Kedarnath", "Badrinath", "Golden Temple", "Gurudwara sahib"],
+    correct: "Golden Temple",
+    questionId: "3542596"
+  },
+  {
+    question: "Aishwarya Rai was crowned as miss world in which year",
+    answers: ["1995", "1994", "1996", "1998"],
+    correct: "1994",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which restaurant's mascot is a clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Green", "Red", "Light Blue", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "In .cpp extension cpp stands for?",
+    answers: ["C plus plus", "C prime plus", "C plus prime", "Plus plus c"],
+    correct: "C plus plus",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Who was the prime minister of india in 2019 ?",
+    answers: ["Rahul gandhi", "Amit Shah", "Mayawati", "Narendra modi"],
+    correct: "Narendra modi",
+    questionId: "4235063"
+  },
+  {
+    question: "Heighest peak of the mountain Himalaya is?",
+    answers: ["Mount Everest", "K2", "Makalu", "Fuji"],
+    correct: "Mount Everest",
+    questionId: "4321002"
+  },
+  {
+    question: 'Who sung the song "Aye dil hai muskil"?',
+    answers: [
+      "Vishal dadlani",
+      "Arijit Singh",
+      "Ranbir kappor",
+      "Alia bhatt"
+    ],
+    correct: "Arijit Singh",
+    questionId: "4442286"
+  },
+  {
+    question: "ALU stands for?",
+    answers: [
+      "Airthmatic logic Unit",
+      "Aao Laao Uthao",
+      "Airthmetic unit logic",
+      "Unit of airthmetic logic"
+    ],
+    correct: "Airthmatic logic Unit",
+    questionId: "4564430"
+  },
+  {
+    question: "Who is known as Father of Nation?",
+    answers: ["Mahatma Gandhi", "Rahul Gandhi", "Sanjay Gandhi", "Sonia Gandhi"],
+    correct: "Mahatma Gandhi",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "National flower of india is?",
+    answers: ["Rose", "Lily", "Lotus", "Mariegold"],
+    correct: "Lotus",
+    questionId: "4795960"
+  },
+  {
+    question: "Which youtoube chanel has the highest subscriber in the world?",
+    answers: ["T series", "BB ki vines", "Pewdipie", "Snickers"],
+    correct: "T series",
+    questionId: "4811162"
+  },
+  {
+    question: "Which indian city is known as It hub?",
+    answers: ["Delhi", "Ballia", "Jaipur", "Banglore"],
+    correct: "Banglore",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
